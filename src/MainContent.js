a938288f1c435d6f9bfc7d36023f42fc6e919b74
Devam Patel <devampatel7@gmail.com>
2021-07-30 15:11:58 -0400

    fixed the form part

--- src/MainContent.js
+++ src/MainContent.js
@@ -8,7 +8,9 @@ const MainContent = () => {
     const [isSubmit, setIsSubmit] = useState(false);
     const [isdestSubmit, setDestSubmit] = useState(false);
     const [isdateSubmit, setDateSubmit] = useState(false);
+ 
 
+    
     const handleChange = e => {
         if(e.target.name === 'dest')
         setDestination(e.target.value);
@@ -18,25 +20,25 @@ const MainContent = () => {
     }
 
     const handleSubmit = e => {
-        if(e.target.name ==='destButton'){
-            setDestSubmit(true);
-        }
-        else{
-            setDateSubmit(true);
-        }
-        if(isdateSubmit && isdateSubmit) setIsSubmit(true);
+       setIsSubmit(true);
+       e.preventDefault();
     }
     return (
+        
         <div>
             
-                <input name='dest' type='text' value={destination} onChange={handleChange} />
-                <button name='destButton'  onClick={handleSubmit}>Submit Destination</button>
-                <input name='date' type='text' value={date} onChange={handleChange}/>
-                <button name='dateButton' onClick={handleSubmit}>Submit Date</button>
+              <form onSubmit={handleSubmit}>
+                <label>Destination</label>
+                <input name='dest' type='text' value={destination} required='true' onChange={handleChange} />
+                <br/>
+                <label>Date</label>
+                <input name='date' type='text' value={date} required='false' onChange={handleChange}/>
+                <br/>
+                <button type='submit'>Submit</button>
+              </form>
 
-            
      
-          {isSubmit ? <h1>The destination is {destination}, on {date}</h1> : <h1>nothing here</h1> }
+          {(destination && date) ? <h1>The destination is {destination}, on {date}</h1> : <h1> Please enter the required forms </h1> }
         </div>
     )
 }
